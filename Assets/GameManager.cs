cffbc50687872cbf5d165e57913ce87db8818beb
aulim-id <dlz.am.id@gmail.com>
2019-02-05 16:17:00 +0700

    - Install DOTween - Finish basic control

---
+++ Assets/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] lanes;
+    [SerializeField] private GameObject player;
+
+    private GameObject spawnedPlayer;
+    public bool inputEnabled;
+    private bool tapStarted;
+    private float startX;
+    private float endX;
+
+    private void Awake()
+    {
+        spawnedPlayer = Instantiate(player);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnedPlayer.GetComponent<PlayerController>().Lanes = lanes;
+        spawnedPlayer.GetComponent<PlayerController>().Initialize(lanes.Length / 2);
+
+        tapStarted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputEnabled)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Debug.Log("Tap start" + Input.mousePosition);
+                startX = Input.mousePosition.x;
+                tapStarted = true;
+            }
+
+            if(Input.GetMouseButtonUp(0) && tapStarted)
+            {
+                //Debug.Log("Tap end" + Input.mousePosition);
+                endX = Input.mousePosition.x;
+                endX -= startX;
+                if (endX < 0)
+                    spawnedPlayer.GetComponent<PlayerController>().MoveLeft();
+                else if (endX > 0)
+                    spawnedPlayer.GetComponent<PlayerController>().MoveRight();
+                tapStarted = false;
+            }
+
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                spawnedPlayer.GetComponent<PlayerController>().MoveLeft();
+            }
+            
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                spawnedPlayer.GetComponent<PlayerController>().MoveRight();
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform[] lanes;
+    private int currentLane;
+
+    public Transform[] Lanes
+    {
+        get { return lanes; }
+        set { lanes = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Initialize(int startingLane)
+    {
+        currentLane = startingLane;
+        transform.position = lanes[currentLane].position;
+    }
+
+    public void MoveLeft()
+    {
+        if (currentLane < 1)
+            return;
+        currentLane -= 1;
+        //transform.position = lanes[currentLane].position;
+        //Debug.Log("Move left" + transform.position);
+        transform.DOMove(lanes[currentLane].position, 0.3f);
+    }
+
+    public void MoveRight()
+    {
+        if (currentLane >= lanes.Length - 1)
+            return;
+        currentLane += 1;
+        //transform.position = lanes[currentLane].position;
+        //Debug.Log("Move right" + transform.position);
+        transform.DOMove(lanes[currentLane].position, 0.3f);
+    }
+}
